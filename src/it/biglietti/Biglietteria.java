a7e0a2509ac7d3321a69d28edaa6fdeb6329b920
RosarioMazzocca <mazzocca.rosario@libero.it>
2021-11-30 17:58:08 +0100

    Bonus completo e aggiunto format data

--- src/it/biglietti/Biglietteria.java
+++ src/it/biglietti/Biglietteria.java
@@ -1,5 +1,6 @@
 package it.biglietti;
 
+// import java.util.InputMismatchException;
 import java.util.Scanner;
 
 //Stanza Zoom 4
@@ -7,19 +8,30 @@ import java.util.Scanner;
 public class Biglietteria {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		boolean flessibile = true;
 		
 		System.out.println("Inserire km: ");
 		int km = scanner.nextInt();
 		System.out.println("Inserire età: ");
 		int eta = scanner.nextInt();
 		
-		Biglietto b = new Biglietto(km, eta);
+		Biglietto b = new Biglietto(km, eta, flessibile);
+		
+			do {
+				System.out.println("Scrivere 'true' per biglietto flessibile o 'false' per biglietto normale");
+				flessibile = scanner.nextBoolean();
+				b.setFlessibile(flessibile);
+				b.isValidFlessibile(); 
+				}
+			while (flessibile != true && flessibile != false); 
+		
+			System.out.println("Data odierna: " + b.getDataFormattata());
+		System.out.println("Scadenza biglietto: " + b.calcolaDataScadenza());
 		
 		try {
 			System.out.println("Prezzo finale del biglietto: " + b.CalcolaPrezzo());
 		} catch (Exception c) {
 			System.out.println("Errore" + c.getMessage());
-			
 		}
 		
 		scanner.close();
--- src/it/biglietti/Biglietto.java
+++ src/it/biglietti/Biglietto.java
@@ -1,5 +1,8 @@
 package it.biglietti;
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.InputMismatchException;
 
 // Stanza Zoom 4
 
@@ -9,18 +12,27 @@ public class Biglietto {
 	
 	private int kmDaPercorrere;
 	private int etaPasseggero;
+	private LocalDate data;
+	private boolean flessibile;
+	private String dataFormattata;
 	
 	// Costanti
 	
 	private final BigDecimal COSTO_PER_KM = new BigDecimal("0.21"); // decimals vuole new per crearne uno nuovo
 	private final BigDecimal SCONTO_OVER_65 = new BigDecimal("0.4");
 	private final BigDecimal SCONTO_UNDER_18 = new BigDecimal("0.2");
+	private final int DURATA_NORMALE = 30;
+	private final int DURATA_FLESSIBILE = 90;
+	private final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 	
 	// Costruttore
 	
-	public Biglietto(int kmDaPercorrere, int etaPasseggero) {
+	public Biglietto(int kmDaPercorrere, int etaPasseggero, boolean flessibile) {
 		this.kmDaPercorrere = kmDaPercorrere;
 		this.etaPasseggero = etaPasseggero;
+		this.flessibile = flessibile;
+		data = LocalDate.now();
+		dataFormattata = data.format(FORMATTER);
 	}
 	
 	// Getters e Setters
@@ -39,6 +51,18 @@ public class Biglietto {
 
 	public void setEtaPasseggero(int etaPasseggero) {
 		this.etaPasseggero = etaPasseggero;
+	}
+	
+	public boolean isflessibile() {
+		return flessibile;
+	}
+	
+	public void setFlessibile(boolean flessibile) {
+		this.flessibile = flessibile;
+	}
+	
+	public String getDataFormattata() {
+		return dataFormattata;
 	}
 	
 	// Metodi
@@ -62,8 +86,13 @@ public class Biglietto {
 	public BigDecimal CalcolaPrezzo() throws Exception {
 		if (isValidKm()) {
 			BigDecimal kmDaPercorrereBD = BigDecimal.valueOf(kmDaPercorrere); // traduce int in big decimal
+			BigDecimal costoAggiuntivo = BigDecimal.valueOf(1.1);
 			BigDecimal sconto = COSTO_PER_KM.multiply(CalcolaSconto().multiply(kmDaPercorrereBD)); // così sconto diventa 0 se ha un età compresa tra 18 e 64, in modo da non avere sconto
-			return COSTO_PER_KM.multiply(kmDaPercorrereBD).subtract(sconto);
+			if(flessibile) {
+				return (COSTO_PER_KM.multiply(kmDaPercorrereBD).subtract(sconto)).multiply(costoAggiuntivo);
+			} else {
+				return COSTO_PER_KM.multiply(kmDaPercorrereBD).subtract(sconto);
+			}
 		} else {
 			throw new Exception(" Km devono essere maggiore di 0");
 		}
@@ -82,6 +111,28 @@ public class Biglietto {
 			return false;
 		} else {
 			return true;
+		}
+	}
+	
+	public String calcolaDataScadenza() {
+		LocalDate dataScadenza;
+		if(flessibile) {
+			dataScadenza = data.plusDays(DURATA_FLESSIBILE);
+		} else {
+			dataScadenza = data.plusDays(DURATA_NORMALE);
+		}
+		return dataScadenza.format(FORMATTER);
+	}
+	
+	// Errore dal dal compilatore se si inserisce un valore sbagliato al boolean
+	
+	public boolean isValidFlessibile() throws InputMismatchException {
+		if(flessibile) {
+			return true;
+		} else if(!flessibile) {
+			return false;
+		} else {
+			throw new InputMismatchException("Errore: input non valido. Inserire 'true' oppure 'false");
 		}
 	}
 
